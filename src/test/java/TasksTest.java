95297310b94ab7a1123ee7de3e6f0ae5b526197d
Rafael Trojack <rafael.trojack@adp.com>
2022-07-13 18:47:08 -0300

    Adicionando paralelismo e remote web driver

--- src/test/java/TasksTest.java
+++ src/test/java/TasksTest.java
@@ -2,14 +2,17 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class TasksTest {
 
     @Test
-    public void deveInserirTarefaComSucesso() {
+    public void deveInserirTarefaComSucesso() throws MalformedURLException {
         WebDriver driver = getWebDriver();
         try {
             //clicar no botao
@@ -31,7 +34,7 @@ public class TasksTest {
     }
 
     @Test
-    public void naoDeveInserirTarefaSemDescricao() {
+    public void naoDeveInserirTarefaSemDescricao() throws MalformedURLException {
         WebDriver driver = getWebDriver();
         try {
             //clicar no botao
@@ -53,7 +56,7 @@ public class TasksTest {
     }
 
     @Test
-    public void naoDeveInserirTarefaSemData() {
+    public void naoDeveInserirTarefaSemData() throws MalformedURLException {
         WebDriver driver = getWebDriver();
         try {
             //clicar no botao
@@ -75,7 +78,7 @@ public class TasksTest {
     }
 
     @Test
-    public void naoDeveInserirTarefaComDataAnterior() {
+    public void naoDeveInserirTarefaComDataAnterior() throws MalformedURLException {
         WebDriver driver = getWebDriver();
         try {
             //clicar no botao
@@ -96,9 +99,10 @@ public class TasksTest {
         }
     }
 
-    private WebDriver getWebDriver() {
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://localhost:8001/tasks");
+    private WebDriver getWebDriver() throws MalformedURLException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+        driver.navigate().to("http://192.168.0.105:8001/tasks");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         return driver;
     }
